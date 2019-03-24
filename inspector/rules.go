362477bb5b404b9c0674125c04cfa1d2153cd1b5
Jim Filippou <jimfilippou8@gmail.com>
2019-03-24 03:58:48 +0200

    add borders

--- inspector/rules.go
+++ inspector/rules.go
@@ -1,7 +1,20 @@
 package inspector
 
 func rules() map[string]string {
+	// Create the rules Map
 	r := make(map[string]string)
+
+	// Borders
+	r["border: 1px solid #dee2e6"] = "border"
+	r["border-top: 1px solid #dee2e6"] = "border-top"
+	r["border-bottom: 1px solid #dee2e6"] = "border-bottom"
+	r["border-right: 1px solid #dee2e6"] = "border-right"
+	r["border-left: 1px solid #dee2e6"] = "border-left"
+
+	// Border radius
+	r["border-radius: .25rem"] = "border"
+
+	// Typography
 	r["font-weight:bold"] = "text-bold"
 	return r
 }
